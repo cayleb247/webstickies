e2ae39a95c460201b3453310994256b5994e06b8
cayleb247 <156046197+cayleb247@users.noreply.github.com>
2025-03-01 13:17:12 -0500

    Create note editor

--- popup/popup.js
+++ popup/popup.js
@@ -2,3 +2,20 @@ function Note(text, color) {
     this.text = text;
     this.color = color;
 }
+
+const popupBody = document.querySelector("popup-body");
+
+
+function displayNoteEditor() {
+    const noteEditor = document.createElement("div");
+    noteEditor.setAttribute("class", "note-editor")
+    popupBody.prepend(noteEditor)
+}
+
+const newNote = document.querySelector(".new-note");
+
+newNote.addEventListener("click", () => {
+
+})
+
+
